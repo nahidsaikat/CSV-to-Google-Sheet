857e976f95db157523b2113c0a1db7d886ed9ba5
nahid_craftsmen <nahidur.rahman@m2amedia.tv>
2020-04-12 21:16:48 +0600

    Add all the files

---
+++ upload.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(credentials)
+
+spreadsheet = client.open('CSV-to-Google-Sheet')
+
+with open('data.csv', 'r') as file_obj:
+    content = file_obj.read()
+    client.import_csv(spreadsheet.id, data=content)
